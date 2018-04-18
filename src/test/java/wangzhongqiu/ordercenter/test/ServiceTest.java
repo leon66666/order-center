a64347564c2a91bd62931ea7816f971fcdc3d3f6
wangzhongqiu <1902518544@qq.com>
2018-04-18 16:00:00 +0800

    first version

---
+++ src/test/java/wangzhongqiu/ordercenter/test/ServiceTest.java
@@ -0,0 +1,29 @@
+package wangzhongqiu.ordercenter.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import wangzhongqiu.spring.redis.service.RedisCommonService;
+
+/**
+ * @author wangzhongqiu
+ * @date 2017/11/30.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring-redis.xml"})
+public class ServiceTest {
+    @Autowired
+    private RedisCommonService redisCommonService;
+
+    @Test
+    public void test() throws Exception {
+        try {
+            redisCommonService.set("loan:1", "1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
