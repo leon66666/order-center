d25a789ec0b66659956979fc9acabef53ae88477
wangzhongqiu <1902518544@qq.com>
2018-04-18 16:00:04 +0800

    first version

--- src/test/java/wangzhongqiu/ordercenter/test/ServiceTest.java
+++ src/test/java/wangzhongqiu/ordercenter/test/ServiceTest.java
@@ -1,29 +1,38 @@
 package wangzhongqiu.ordercenter.test;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import wangzhongqiu.spring.redis.service.RedisCommonService;
+import wangzhongqiu.ordercenter.bo.OrderBo;
+import wangzhongqiu.ordercenter.service.OrderCenterService;
 
 /**
  * @author wangzhongqiu
  * @date 2017/11/30.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/spring-redis.xml"})
+@ContextConfiguration(locations = {"classpath:/spring-ordercenter.xml"})
 public class ServiceTest {
     @Autowired
-    private RedisCommonService redisCommonService;
+    private OrderCenterService orderCenterService;
 
     @Test
     public void test() throws Exception {
-        try {
-            redisCommonService.set("loan:1", "1");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String orderNo = "123243sdf3e4f4344";
+        JSONObject stateInputJSON = new JSONObject();
+        stateInputJSON.put("orderNo", "111");
+        stateInputJSON.put("detailNo", "111");
+        stateInputJSON.put("userId", "111");
+        stateInputJSON.put("loanId", "111");
+        stateInputJSON.put("amount", "111");
+        stateInputJSON.put("couponId", "111");
+        stateInputJSON.put("isAuto", "111");
+        stateInputJSON.put("subPointId", "111");
+        orderCenterService.newOrder(new OrderBo(orderNo, "LOAN_BID", "INIT", stateInputJSON.toJSONString()));
+        orderCenterService.processOrder(orderNo);
     }
 
 }
